d3fe918505c594257bcde4ff01523adec75cf451
Raiya <rijessan@uwaterloo.ca>
2026-06-07 13:16:55 -0400

    random images

--- script.js
+++ script.js
@@ -8,6 +8,31 @@ async function loadSection(id, file) {
   document.getElementById(id).innerHTML = html;
 }
 
+const images = [
+  "assets/Character_1.png",
+  "assets/Character_2.png",
+  "assets/Character_3.png",
+  "assets/Character_4.png"
+];
+
+function setHeroImage() {
+  const imgEl = document.getElementById("heroImage");
+
+  const hasVisited = localStorage.getItem("hasVisited");
+
+  if (!hasVisited) {
+    // FIRST EVER VISIT → always pick first image
+    imgEl.src = images[0];
+
+    localStorage.setItem("hasVisited", "true");
+  } else {
+    // AFTER THAT → random every reload
+    const randomIndex = Math.floor(Math.random() * images.length);
+    imgEl.src = images[randomIndex];
+  }
+}
+
+
 window.addEventListener("DOMContentLoaded", async () => {
   await loadSection("hero", "sections/hero.html");
   await loadSection("about", "sections/about.html");
@@ -16,4 +41,5 @@ window.addEventListener("DOMContentLoaded", async () => {
   await loadSection("projects", "sections/projects.html");
   await loadSection("certs", "sections/certs.html");
   await loadSection("contact", "sections/contact.html");
+  setHeroImage();
 });
